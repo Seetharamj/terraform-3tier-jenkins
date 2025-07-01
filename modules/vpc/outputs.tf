4ff7196708fb0630b4d576c1fff58df95ca7d2ba
seetharam j <seetharamj704@gmail.com>
2025-07-01 17:32:22 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,10 +2,10 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "public_subnet_1_id" {
-  value = aws_subnet.public[0].id
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
 }
 
-output "public_subnet_2_id" {
-  value = aws_subnet.public[1].id
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
 }
