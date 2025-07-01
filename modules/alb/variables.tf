8ec70de54867f22da2e0330d51694943eb00828c
seetharam j <seetharamj704@gmail.com>
2025-07-01 15:55:32 +0530

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,19 +1,19 @@
 variable "project" {
-  description = "Prefix for resource names"
+  description = "Project name"
   type        = string
 }
 
 variable "vpc_id" {
-  description = "VPC ID where the ALB and SG will be created"
+  description = "VPC ID"
   type        = string
 }
 
 variable "public_subnet_ids" {
-  description = "List of public subnet IDs for ALB"
+  description = "List of public subnet IDs across 2 AZs"
   type        = list(string)
 }
 
 variable "asg_name" {
-  description = "Name of the Auto Scaling Group to attach to the Target Group"
+  description = "Name of the Auto Scaling Group"
   type        = string
 }
