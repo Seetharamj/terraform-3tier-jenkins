3c0e57651d247a498b02081e7f45d59a67a6ee72
seetharam j <seetharamj704@gmail.com>
2025-07-01 18:24:23 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,4 @@
+# DELETE THIS ENTIRE RESOURCE BLOCK
 resource "aws_security_group" "app_sg" {
   name        = "${var.project}-app-sg"
   description = "Allow HTTP and SSH"
@@ -26,45 +27,5 @@ resource "aws_security_group" "app_sg" {
 
   tags = {
     Name = "${var.project}-app-sg"
-  }
-}
-
-resource "aws_launch_template" "app" {
-  name_prefix   = "${var.project}-lt-"
-  image_id      = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  network_interfaces {
-    associate_public_ip_address = false
-    subnet_id                   = var.private_subnet_ids[0]
-    security_groups             = [aws_security_group.app_sg.id]
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name = "${var.project}-ec2"
-    }
-  }
-}
-
-resource "aws_autoscaling_group" "app_asg" {
-  name                = "${var.project}-asg"
-  min_size            = var.min_size
-  max_size            = var.max_size
-  desired_capacity    = var.desired_capacity
-  vpc_zone_identifier = var.private_subnet_ids
-  health_check_type   = "EC2"
-
-  launch_template {
-    id      = aws_launch_template.app.id
-    version = "$Latest"
-  }
-
-  tag {
-    key                 = "Name"
-    value               = "${var.project}-asg"
-    propagate_at_launch = true
   }
 }
