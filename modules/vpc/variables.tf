c79cfd02fd35a9620a4794aa935c09adc2053904
seetharam j <seetharamj704@gmail.com>
2025-07-01 11:52:57 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,24 +1,24 @@
 variable "project" {
-  description = "Project name prefix"
+  description = "Project name prefix for tagging AWS resources"
   type        = string
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for VPC"
+  description = "CIDR block for the VPC"
   type        = string
 }
 
 variable "public_subnet_cidr" {
-  description = "CIDR block for public subnet"
+  description = "CIDR block for the public subnet"
   type        = string
 }
 
 variable "private_subnet_cidr" {
-  description = "CIDR block for private subnet"
+  description = "CIDR block for the private subnet"
   type        = string
 }
 
 variable "az" {
-  description = "Availability Zone"
+  description = "AWS Availability Zone to deploy the subnets (e.g., us-east-1a)"
   type        = string
 }
