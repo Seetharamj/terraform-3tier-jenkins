047f73b15f908a5236935fccf4297f2df41c34c9
seetharam j <seetharamj704@gmail.com>
2025-07-01 11:00:36 +0530

    Create variables.tf

---
+++ modules/alb/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  description = "Project name prefix"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
+
+variable "asg_name" {
+  description = "Name of EC2 Auto Scaling Group"
+  type        = string
+}
