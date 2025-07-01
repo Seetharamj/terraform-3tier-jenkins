cb2ea77e17bef028739736521940cfccdeab85ff
seetharam j <seetharamj704@gmail.com>
2025-07-01 17:31:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,30 +6,33 @@ variable "project" {
 variable "region" {
   description = "AWS region"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
-# ✅ Updated to support multiple AZs and subnets
 variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for public subnets"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for private subnets"
   type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "azs" {
   description = "List of Availability Zones"
   type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
 }
 
-# EC2 Variables
 variable "ami_id" {
   description = "AMI ID for EC2 instances"
   type        = string
@@ -38,6 +41,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
+  default     = "t3.micro"
 }
 
 variable "key_name" {
@@ -45,25 +49,28 @@ variable "key_name" {
   type        = string
 }
 
-# RDS Variables
 variable "rds_engine" {
   description = "RDS database engine"
   type        = string
+  default     = "mysql"
 }
 
 variable "rds_engine_version" {
   description = "RDS engine version"
   type        = string
+  default     = "8.0"
 }
 
 variable "rds_instance_class" {
   description = "RDS instance class"
   type        = string
+  default     = "db.t3.micro"
 }
 
 variable "rds_port" {
   description = "RDS port number"
   type        = number
+  default     = 3306
 }
 
 variable "db_username" {
