a9b3fc1c3b4e9ccfc2c531449cd32c002c7c241a
seetharam j <seetharamj704@gmail.com>
2025-07-01 18:05:09 +0530

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,3 +1,9 @@
 output "asg_name" {
-  value = aws_autoscaling_group.app_asg.name
+  description = "The name of the Auto Scaling Group"
+  value       = aws_autoscaling_group.app_asg.name
+}
+
+output "ec2_sg_id" {
+  description = "Security Group ID for EC2 instances"
+  value       = aws_security_group.app_sg.id
 }
