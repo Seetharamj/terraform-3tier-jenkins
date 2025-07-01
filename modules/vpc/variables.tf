cc2363dcb76b3641c44934c381f79dce025f40bb
seetharam j <seetharamj704@gmail.com>
2025-07-01 10:16:11 +0530

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "project" {
+  description = "Project name prefix"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for private subnet"
+  type        = string
+}
+
+variable "az" {
+  description = "Availability Zone"
+  type        = string
+}
