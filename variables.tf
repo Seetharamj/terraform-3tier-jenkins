93caecdc57bc61df43201aeea8bdd3f88b2a55b5
seetharam j <seetharamj704@gmail.com>
2025-07-04 10:27:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -101,14 +101,3 @@ variable "desired_capacity" {
   default     = 1
 }
 
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
-}
